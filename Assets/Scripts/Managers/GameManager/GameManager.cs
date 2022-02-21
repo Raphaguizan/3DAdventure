18cf5d3c768f36e588fecc68eaade334c5166d0c
DESKTOP-Q313TGL\rapha <raphaelguizan@gmail.com>
2022-02-21 17:29:34 -0300

    feat: game manager

---
+++ Assets/Scripts/Managers/GameManager/GameManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Game.Util;
+using Game.StateMachine;
+
+public class GameManager : Singleton<GameManager>
+{
+    public enum GameStates
+    {
+        MENU,
+        GAME,
+        PAUSE,
+        WIN,
+        LOSE
+    }
+    public StateMachineBase<GameStates> StateMachine => _stateMachine;
+
+    private StateMachineBase<GameStates> _stateMachine;
+
+    private void Start()
+    {
+        Init();
+    }
+    
+    public void Init()
+    {
+        _stateMachine = new StateMachineBase<GameStates>();
+        _stateMachine.SwitchState(GameStates.MENU);
+    }
+}
